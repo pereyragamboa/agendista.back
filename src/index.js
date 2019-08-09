5ada87b122f79ebf5f6bea18bc026924f410d5d3
Martin Pereyra <pereyra.gamboa@outlook.com>
2019-08-08 19:02:09 -0500

    Implement service schema as .graphql

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
-const { ApolloServer, gql } = require('apollo-server');
-const { serviceRoot, serviceSchema } = require('./services');
+const { ApolloServer } = require('apollo-server');
+const services = require('./services');
 const clients = require('./clients/index');
 
-const servicesServer = new ApolloServer({
-  typeDefs: gql(serviceSchema),
-  resolvers: {
-    Query: {
-      getService: (parent, args) => serviceRoot.getService(args),
-      getAllServices: serviceRoot.getAllServices,
-    },
-    Mutation: {
-      addService: (parent, args) => serviceRoot.addService(args),
-      deleteService: (parent, args) => serviceRoot.deleteService(args),
-      updateService: (parent, args) => serviceRoot.updateService(args)
-    }
-  }
-});
-
 const clientsServer = new ApolloServer(clients);
+const servicesServer = new ApolloServer(services);
 
 let port = 4000;
 try {
--- src/services/index.js
+++ src/services/index.js
@@ -1,18 +1,18 @@
-const os = require('os');
-
-const mutations = require('./mutations');
-const queries = require('./queries');
+const { importSchema } = require('graphql-import');
 const resolvers = require('./resolvers');
-const { types } = require('./types');
 
-const serviceSchema = [mutations, queries, types].join(os.EOL.toString());
+const typeDefs = importSchema('src/services/schema.graphql');
 
-const serviceRoot = {
-  addService: (args) => resolvers.addService(args.newService),
-  deleteService: (args) => resolvers.deleteService(args.serviceId),
-  getAllServices: resolvers.getAllServices,
-  getService: (args) => resolvers.getService(args.serviceId),
-  updateService: (args) => resolvers.updateService(args.serviceId, args.service),
+const clientResolvers = {
+  Query:{
+    getAllServices: resolvers.getAllServices,
+    getService: (parent, args) => resolvers.getService(args.serviceId),
+  },
+  Mutation:{
+    addService: (parent, args) => resolvers.addService(args.newService),
+    deleteService: (parent, args) => resolvers.deleteService(args.serviceId),
+    updateService: (parent, args) => resolvers.updateService(args.serviceId, args.service),
+  }
 };
 
-module.exports = { serviceRoot, serviceSchema };
+module.exports = { typeDefs, resolvers: clientResolvers };
