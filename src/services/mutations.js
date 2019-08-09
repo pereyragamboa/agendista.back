4d5dff847301513ebd4192076b6fa1aafbe8fa0f
Martin Pereyra <pereyra.gamboa@outlook.com>
2019-08-08 19:03:43 -0500

    Remove service schema code

--- src/services/mutations.js
+++
@@ -1,7 +0,0 @@
-const serviceMutations = `type Mutation{
-  addService (newService: ServiceInput!): Service
-  updateService (serviceId: ID!, service: ServiceInput!): Service
-  deleteService (serviceId: ID!): Boolean!
-}`;
-
-module.exports = serviceMutations;
