e029d8213e6a1306d0a19a32573a76ff90a68f0d
Martin Pereyra <pereyra.gamboa@outlook.com>
2019-08-26 18:22:49 -0500

    Implement federated gateway

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { ApolloGateway } = require('@apollo/gateway');
 const { ApolloServer } = require('apollo-server');
 const { buildFederatedSchema } = require('@apollo/federation');
 const services = require('./services');
@@ -11,11 +12,27 @@ const servicesServer = new ApolloServer({
 });
 
 let port = 4000;
-try {
-  [servicesServer, clientsServer].forEach(async server => {
+const serviceList = [];
+const start = async () =>
+{
+  serviceList.push(await startServer(servicesServer, "services"));
+  serviceList.push(await startServer(clientsServer, "clients"));
+  const gateway = new ApolloGateway({ serviceList });
+  const server = new ApolloServer({gateway, subscriptions: false});
+  const result = await server.listen();
+  console.log(`GraphQL main server (${result.url}) started at ${new Date().toLocaleTimeString()}.`);
+};
+start();
+
+async function startServer(server, serverTag) {
+  try {
     const result = await server.listen(++port);
-    console.log(`GraphQL server ${result.url} started at ${new Date().toLocaleTimeString()}.`);
-  });
-} catch (err) {
-  console.log(` ERR > ${err}`);
+    console.log(`GraphQL server '${serverTag}' (${result.url}) started at ${new Date().toLocaleTimeString()}.`);
+    return {
+      name: serverTag,
+      url: result.url
+    };
+  } catch (err) {
+    console.log(` ERR > ${err}`);
+  }
 }
