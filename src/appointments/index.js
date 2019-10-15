ebb0317b412dcfa2f413f923104cf79ea4fd1695
Martin Pereyra <pereyra.gamboa@outlook.com>
2019-10-14 19:39:13 -0500

    Add appointments index file

---
+++ src/appointments/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const gql = require('apollo-server');
+const appointmentResolvers = require('./resolvers');
+
+module.exports = {
+  typeDefs: gql(fs.readFileSync("/src/appointments/schema.graphqls").toString()),
+  resolvers: {
+    Query: {
+      getAppointment: (_, args) => appointmentResolvers.getAppointment(args.appointmentId),
+      getCustomerAppointments: (_, args) => appointmentResolvers.getCustomerAppointments(args.customerId),
+      getProfileAppointments: (_, args) => appointmentResolvers.getProfileAppointments(args.profileId),
+    },
+    Mutation: {
+      addAppointment: (_, args) => appointmentResolvers.addAppointment(args.appointment),
+      cancelAppointment: (_, args) => appointmentResolvers.cancelAppointment(args.appointmentId),
+      updateAppointment: (_, args) => appointmentResolvers.updateAppointment(args.appointmentId, args.appointment)
+    }
+  }
+};
